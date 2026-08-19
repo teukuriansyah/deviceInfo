8eec29ceb612ee161501cfe4ab7cfe02cecd484a
riansyah24 <jackrian72@gmail.com>
2026-08-19 19:44:37 +0700

    fix: fix device info model

--- android/src/main/java/com/testing/plugins/deviceinfo/deviceInfoPlugin.java
+++ android/src/main/java/com/testing/plugins/deviceinfo/deviceInfoPlugin.java
@@ -1,140 +1,142 @@
 package com.testing.plugins.deviceinfo;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.os.Build;
-import android.util.DisplayMetrics;
-import android.view.WindowMetrics;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import android.util.DisplayMetrics;
+import android.app.ActivityManager;
+import android.content.Context;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import android.os.Build;
 
 @CapacitorPlugin(name = "deviceInfo")
 public class deviceInfoPlugin extends Plugin {
 
-    // Manufacturer
-    @PluginMethod
-    public void deviceInfoManufacture(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("manufacture", Build.MANUFACTURER);
-        call.resolve(ret);
-    }
+  //Manufacture
+  @PluginMethod
+  public void deviceInfoManufacture(PluginCall call) {
+    String manufacturer = Build.MANUFACTURER;
 
-    // Brand
-    @PluginMethod
-    public void deviceInfoBrand(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("brand", Build.BRAND);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("manufacture",manufacturer);
+    call.resolve(ret);
+  }
 
-    // Model
-    @PluginMethod
-    public void deviceInfoModel(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("model", Build.MODEL);
-        call.resolve(ret);
-    }
+  //Brand
+  @PluginMethod
+  public void deviceInfoBrand(PluginCall call) {
+    String brand = Build.BRAND;
 
-    // Board
-    @PluginMethod
-    public void deviceInfoBoard(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("board", Build.BOARD);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("brand",brand);
+    call.resolve(ret);
+  }
+  
+  //Model
+  @PluginMethod
+  public void deviceInfoModel(PluginCall call) {
+    String model = Build.MODEL;
 
-    // Hardware
-    @PluginMethod
-    public void deviceInfoHardware(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("hardware", Build.HARDWARE);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("model",model);
+    call.resolve(ret);
+  }
+  
+  //Board
+  @PluginMethod
+  public void deviceInfoBoard(PluginCall call) {
+    String board = Build.BOARD;
 
-    // OS Version
-    @PluginMethod
-    public void deviceInfoOSVersion(PluginCall call) {
-        JSObject ret = new JSObject();
-        ret.put("osVersion", Build.VERSION.RELEASE);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("board",board);
+    call.resolve(ret);
+  }
+  
+  //Hardware
+  @PluginMethod
+  public void deviceInfoHardware(PluginCall call) {
+    String hardware = Build.HARDWARE;
 
-    // Width
-    @PluginMethod
-    public void deviceWidth(PluginCall call) {
-        int width;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowMetrics windowMetrics = getActivity().getWindowManager().getCurrentWindowMetrics();
-            width = windowMetrics.getBounds().width();
-        } else {
-            DisplayMetrics metrics = new DisplayMetrics();
-            getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            width = metrics.widthPixels;
-        }
+    JSObject ret = new JSObject();
+    ret.put("hardware",hardware);
+    call.resolve(ret);
+  }
+  
+  //OS Version
+  @PluginMethod
+  public void deviceInfoOSVersion(PluginCall call) {
+    String osVersion = Build.VERSION.RELEASE;
 
-        JSObject ret = new JSObject();
-        ret.put("width", width);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("osVersion",osVersion);
+    call.resolve(ret);
+  }
+  
+  //Width
+  @PluginMethod
+  public void deviceWidth(PluginCall call) {
+    DisplayMetrics metrics = new DisplayMetrics();
+    getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-    // Height
-    @PluginMethod
-    public void deviceHeight(PluginCall call) {
-        int height;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowMetrics windowMetrics = getActivity().getWindowManager().getCurrentWindowMetrics();
-            height = windowMetrics.getBounds().height();
-        } else {
-            DisplayMetrics metrics = new DisplayMetrics();
-            getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            height = metrics.heightPixels;
-        }
+    int width = metrics.widthPixels;
 
-        JSObject ret = new JSObject();
-        ret.put("height", height);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("width",width);
+    call.resolve(ret);
+  }
+  
+  //Height
+  @PluginMethod
+  public void deviceHeight(PluginCall call) {
+    DisplayMetrics metrics = new DisplayMetrics();
+    getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-    // Density DPI
-    @PluginMethod
-    public void deviceDensity(PluginCall call) {
-        DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+    int height = metrics.heightPixels;
 
-        JSObject ret = new JSObject();
-        ret.put("density", metrics.densityDpi);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("height",height);
+    call.resolve(ret);
+  }
+  
+  //Density DPI
+  @PluginMethod
+  public void deviceDensity(PluginCall call) {
+    DisplayMetrics metrics = new DisplayMetrics();
+    getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-    // Total RAM
-    @PluginMethod
-    public void deviceTotalRam(PluginCall call) {
-        ActivityManager actManager = (ActivityManager) getContext().getSystemService(Context.ACTIVITY_SERVICE);
-        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
-        
-        if (actManager != null) {
-            actManager.getMemoryInfo(memInfo);
-        }
+    int density = metrics.densityDpi;
 
-        JSObject ret = new JSObject();
-        ret.put("totalRam", memInfo.totalMem);
-        call.resolve(ret);
-    }
+    JSObject ret = new JSObject();
+    ret.put("density",density);
+    call.resolve(ret);
+  }
+  
+  //Total RAM
+  @PluginMethod
+  public void deviceTotalRam(PluginCall call) {
+    ActivityManager actManager = (ActivityManager) getContext().getSystemService(Context.ACTIVITY_SERVICE);
+    ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+    actManager.getMemoryInfo(memInfo);
 
-    // Free RAM
-    @PluginMethod
-    public void deviceAvailableRam(PluginCall call) {
-        ActivityManager actManager = (ActivityManager) getContext().getSystemService(Context.ACTIVITY_SERVICE);
-        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+    long totalRam = memInfo.totalMem;
+    
+    JSObject ret = new JSObject();
+    ret.put("totalRam",totalRam);
+    call.resolve(ret);
+  }
+  
+  //Free RAM
+  @PluginMethod
+  public void deviceAvailableRam(PluginCall call) {
+    ActivityManager actManager = (ActivityManager) getContext().getSystemService(Context.ACTIVITY_SERVICE);
+    ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+    actManager.getMemoryInfo(memInfo);
 
-        if (actManager != null) {
-            actManager.getMemoryInfo(memInfo);
-        }
-
-        JSObject ret = new JSObject();
-        ret.put("availableRam", memInfo.availMem);
-        call.resolve(ret);
-    }
+    long availableRam = memInfo.availMem;
+    
+    JSObject ret = new JSObject();
+    ret.put("availableRam",availableRam);
+    call.resolve(ret);
+  }
 }
